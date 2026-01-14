c21824de63be9a6c0999eda091572ae5b53f8930
Yassir <atertay@gmail.com>
2026-01-14 15:44:22 +0100

    Update styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from 'next';
-import SmoothScrollGSAP from '../components/ui/smoothscroll';
-import './globals.css';
+import type { Metadata } from "next";
+import SmoothScrollGSAP from "../components/ui/smoothscroll";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Patrick Lyons',
-  description: 'Fitness Coach from Austin, Texas',
+  title: "Patrick Lyons",
+  description: "Fitness Coach from Austin, Texas",
   keywords: [
-    'Patrick Lyons, Fitness coach in Austin, Fitness coach Texas, Fitness online coach',
+    "Patrick Lyons, Fitness coach in Austin, Fitness coach Texas, Fitness online coach",
   ],
-  metadataBase: new URL('https://patrick-lymons.vercel.app/'),
+  metadataBase: new URL("https://patrick-lymons.vercel.app/"),
   openGraph: {
-    type: 'website',
+    type: "website",
     images: [
       {
-        url: '/opengraph-image.png',
+        url: "/opengraph-image.png",
         width: 1200,
         height: 630,
-        alt: 'Patrick Lyons',
-        type: 'image/png',
+        alt: "Patrick Lyons",
+        type: "image/png",
       },
     ],
   },
   twitter: {
-    title: 'Patrick Lyons',
-    description: 'Fitness Coach from Austin, Texas',
-    images: ['/opengraph-image.png'],
+    title: "Patrick Lyons",
+    description: "Fitness Coach from Austin, Texas",
+    images: ["/opengraph-image.png"],
   },
   icons: {
-    icon: './favicon.ico',
+    icon: "./favicon.ico",
   },
 };
 
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased bg-gray-50">
+      <body className="overflow-x-hidden bg-gray-50 antialiased">
         <SmoothScrollGSAP />
         {children}
       </body>
