755a495d692560f89cb2730b97e6217dea0422df
Yassir <atertay@gmail.com>
2026-01-14 11:32:05 +0100

    refactor: Remove the use client and add components to home.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,7 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import { useState } from 'react';
-import Contact from '../components/contact';
-import Faq from '../components/faq';
-import Footer from '../components/footer';
-import Hero from '../components/hero';
-import HowWorks from '../components/how-it-works';
-import About from '../components/meet-your-coach';
-import Method from '../components/method';
-import Navbar from '../components/navbar';
-import Preloader from '../components/preload';
-import ScrollBtn from '../components/scroll-button';
+const HomePage = dynamic(() => import('../components/home'));
 
-export default function Home() {
-  const [loaded, setLoaded] = useState(false);
-
-  return (
-    <>
-      {!loaded && <Preloader onComplete={() => setLoaded(true)} />}
-      {loaded && (
-        <>
-          <div className="min-h-dvh grid grid-rows-[auto_1fr]">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Method />
-          <HowWorks />
-          <Faq />
-          <Contact />
-          <Footer />
-          <ScrollBtn />
-        </>
-      )}
-    </>
-  );
+export default function Page() {
+  return <HomePage />;
 }
