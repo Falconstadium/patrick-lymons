1a5300379c94548a78487a733626b546826fcb45
Yassir <atertay@gmail.com>
2026-01-16 22:56:43 +0100

    refactor: Update the link

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,7 @@
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useRef } from 'react';
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
 
 export default function Contact() {
   const contactRef = useRef(null);
@@ -13,27 +13,27 @@ export default function Contact() {
       autoAlpha: 0,
       y: 100,
       stagger: 0.2,
-      ease: 'power2.out',
+      ease: "power2.out",
     });
 
     ScrollTrigger.create({
       animation: tl,
       trigger: contactRef.current,
-      start: 'top 90%',
-      end: 'bottom bottom',
+      start: "top 90%",
+      end: "bottom bottom",
       scrub: 1,
     });
   });
 
   return (
-    <div className="min-h-[75dvh] py-20 px-5" ref={contactRef} id="contact">
-      <div className="max-w-xl w-full space-y-4 mx-auto grid place-items-center">
+    <div className="min-h-[75dvh] px-5 py-20" ref={contactRef} id="contact">
+      <div className="mx-auto grid w-full max-w-xl place-items-center space-y-4">
         {/* Header */}
         <div className="space-y-3">
-          <h1 className="font-general font-medium text-xs xl:text-sm text-neutral-500">
+          <h1 className="font-general text-xs font-medium text-neutral-500 xl:text-sm">
             Contact
           </h1>
-          <h2 className="text-3xl md:text-5xl font-bold font-general">
+          <h2 className="font-general text-3xl font-bold md:text-5xl">
             Let’s build your plan together!
           </h2>
           <p className="font-gambetta text-sm lg:text-base">
@@ -43,12 +43,14 @@ export default function Contact() {
         </div>
 
         {/* Send Button */}
-        <button
-          onClick={() => window.open('https://ig.me/m/patricklyons', '_blank')}
-          aria-label="Send message on Instagram"
-          className=" bg-[#F9652F] font-medium font-general text-sm py-2 px-5 rounded-lg transition-colors shadow-lg hover:bg-orange-600 active:bg-orange-600">
+        <a
+          href="https://ig.me/m/patricklyons"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-general rounded-lg bg-[#F9652F] px-5 py-2 text-sm font-medium shadow-lg transition-colors hover:bg-orange-600 active:bg-orange-600"
+        >
           Send a Message
-        </button>
+        </a>
       </div>
     </div>
   );
