97861b8aab62dac0926db86b3265b0b1197b1146
Yassir <atertay@gmail.com>
2026-01-18 10:31:15 +0100

    refactor: Add youtube link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import {
   IconBrandFacebook,
   IconBrandInstagram,
   IconBrandLinktree,
+  IconBrandYoutube,
 } from "@tabler/icons-react";
 
 export default function Footer() {
@@ -21,12 +22,12 @@ export default function Footer() {
             Unlock your potential and redefine your limits with structured
             training and nutrition guidance tailored for you.
           </p>
-          <div className="mt-4 flex justify-center gap-4 md:justify-normal">
+          <div className="mt-4 flex justify-center gap-6 md:justify-normal">
             <a
               href="https://web.facebook.com/patrick.lyons.97594/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex h-8 w-8 items-center justify-center rounded-full border border-black"
+              className="flex items-center justify-center"
             >
               <IconBrandFacebook stroke={2} size={24} />
             </a>
@@ -34,15 +35,23 @@ export default function Footer() {
               href="https://www.instagram.com/patricklyons/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex h-8 w-8 items-center justify-center rounded-full border border-black"
+              className="flex items-center justify-center"
             >
               <IconBrandInstagram stroke={2} size={24} />
+            </a>
+            <a
+              href="https://www.youtube.com/patricklyonsofficial"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center"
+            >
+              <IconBrandYoutube stroke={2} size={24} />
             </a>
             <a
               href="https://linktr.ee/patricklyons?fbclid=IwY2xjawPMrQFleHRuA2FlbQIxMABicmlkETE0YW5LTXc0SW9LM3JUWE5oc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHnP5coViAzUgI_TC5v89JQuozTNdqUySPMq6-ufhZuQKi2ToazN8BeeXZGRx_aem_53IIpCNM_et9RVktQfkylw"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex h-8 w-8 items-center justify-center rounded-full border border-black"
+              className="flex items-center justify-center"
             >
               <IconBrandLinktree stroke={2} size={24} />
             </a>
